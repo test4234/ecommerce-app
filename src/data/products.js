bffe213b14b79d39df97993700fe4954d579a6d0
test4234 <t0291668@gmail.com>
2025-05-09 17:23:42 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: "$10",
+    image: "https://images.pexels.com/photos/2454533/pexels-photo-2454533.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  // Add 9 more products with unique ids
+  {
+    id: 2,
+    name: "Product 1",
+    price: "$10",
+    image: "https://images.pexels.com/photos/2233351/pexels-photo-2233351.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    name: "Product 1",
+    price: "$10",
+    image: "https://images.pexels.com/photos/1893556/pexels-photo-1893556.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 4,
+    name: "Product 1",
+    price: "$10",
+    image: "https://images.pexels.com/photos/2233351/pexels-photo-2233351.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 5,
+    name: "Product 1",
+    price: "$10",
+    image: "https://images.pexels.com/photos/2067403/pexels-photo-2067403.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
+
+
+export default products;
